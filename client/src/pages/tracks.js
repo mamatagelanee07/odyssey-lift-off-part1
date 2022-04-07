72f4e71f794875a070783e58f7143b5b796bde72
mamata.gelanee <mamata.gelanee@imaginecurve.com>
2022-04-07 11:32:28 +0100

    Integrate with front end app

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { Layout } from '../components';
+import { Layout, QueryResult } from '../components';
+import { gql, useQuery } from '@apollo/client';
+import TrackCard from '../containers/track-card';
+
+export const TRACKS = gql`
+query TracksForHome {
+  tracksForHome {
+    id
+    title
+    author {
+      name
+      id
+      photo
+    }
+    thumbnail
+    length
+    modulesCount
+  }
+}
+
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+  if (loading) return 'Loading';
+  if (error) return `Error! ${error.message}`
+  return <Layout grid>
+    <QueryResult error={error} loading={loading} data={data}>
+      {data?.tracksForHome?.map((track) => (
+        <TrackCard key={track.id} track={track} />
+      ))}
+    </QueryResult>
+  </Layout>;
 };
 
 export default Tracks;
